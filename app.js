5157f0f58af165427f65eb6b57d7f4eee2981b43
fitzpaky <kfitzpatrick44@gmail.com>
2020-03-26 13:10:37 -0700

    adding phone number and email link

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ $("#submit").on("click", function(){
     alert("clicked");
     $("#formGroupExampleInput").val();
     var body = $("#exampleFormControlTextarea1").val();
-    $(this).attr("href", "mailto:kfitzpatrick44@gmail.com?subject=Mail from Our Site&body=" + body);
+    $(this).attr("href", "mailto:kfitzpatrick44@gmail.com?subject=Inquiry&body=" + body);
 })
