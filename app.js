f76843288179adad91eb462fd007b6169104eee9
fitzpaky <kfitzpatrick44@gmail.com>
2020-06-06 14:26:32 -0700

    adding materialize

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 $("#submit").on("click", function(){
     alert("clicked");
-    $("#formGroupExampleInput").val();
-    var body = $("#exampleFormControlTextarea1").val();
+    $("#first_name").val();
+    $("#email").val();
+    var body = $("#icon_prefix2").val();
     $(this).attr("href", "mailto:kfitzpatrick44@gmail.com?subject=Inquiry&body=" + body);
 })
